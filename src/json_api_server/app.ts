3e305fca99fa3e7e3a81c15cbd30c7dd762f8369
Manuel Pucher <manuel@outlook.at>
2022-02-13 15:38:36 +0100

    added environment variables

--- src/json_api_server/app.ts
+++ src/json_api_server/app.ts
@@ -1,8 +1,14 @@
+import { processenv } from 'processenv';
 import * as WeatherStation from '..'
+
+const environment_NODE_ENV = String( processenv('NODE_ENV') ?? 'production' );
+const environment_SERIAL_COM_PORT = String( processenv('SERIAL_COM_PORT') ?? '/dev/ttyUSB0' )
+const environment_HTTP_SERVER_PORT = Number( processenv('HTTP_SERVER_PORT') ?? 3000 );
+const environment_LOG_DEBUG = Boolean( processenv('LOG_DEBUG') ?? false );
 
 const myWeatherStation = new WeatherStation.ElsnerP03 ( {
     instanceName: 'WeatherStation',
-    comPort: '/dev/ttyUSB0',
-    httpPort: 3000,
-    log_cmdLine: true
+    comPort: environment_SERIAL_COM_PORT,
+    httpPort: environment_HTTP_SERVER_PORT,
+    log_cmdLine: environment_LOG_DEBUG
 })
